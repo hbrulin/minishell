e0403f21fbfda014f73a2a2c0e8d26f6456d8bf4
Helene BRULIN <hbrulin@e2r7p14.42.fr>
2020-02-03 16:54:06 +0100

    comments

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 14:12:38 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/02/03 16:42:15 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/02/03 16:53:56 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,9 @@ void	prompt(void)
 		ft_putstr("minishell > ");
 		if (get_next_line(STDIN_FILENO, &input) == -1)
 			ft_putstr("Error reading stdin"); //systeme d'erreurs a faire
-		//printf("%s\n", input);
-		trim = ft_strtrim(input, " "); //autres caracteres a afficher?
-		free(input); //on set to NULL en plus?
-		args = parser(trim); //FAIRE UN PARSER
+		trim = ft_strtrim(input, " ");
+		free(input); //set to NULL?
+		args = parser(trim);
 		free(trim);
 		status = run_dmc(args);
 		ft_tabdel((void **)args);
@@ -40,7 +39,7 @@ void	prompt(void)
 int			main(int ac, char **av, char **envp)
 {
 	(void)ac; 
-	(void)av;//comme ca je n'ai que envp
+	(void)av;
 	env = copy_tab(envp);
 	prompt();
 
