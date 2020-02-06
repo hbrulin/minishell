6545811723e6f859c72ccef3a15741a88fa47005
Paloma MOUHALI <pmouhali@e2r6p14.42.fr>
2020-02-06 13:31:36 +0100

    del escape_seq.h cleararg basique presque ok

--- expand_and_escape.c
+++ expand_and_escape.c
@@ -6,7 +6,7 @@
 /*   By: pmouhali <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 17:32:25 by pmouhali          #+#    #+#             */
-/*   Updated: 2020/02/05 19:23:36 by pmouhali         ###   ########.fr       */
+/*   Updated: 2020/02/06 13:29:53 by pmouhali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,72 @@
 **	args[0] = cmd name
 **	DR replace by corresponding env variable
 **	BS replace by corresponding char
+**	Check for syntax error
 */
 
-int	expand_and_escape(char **arg)
+// hello"hello\"""hello"
+// 012345678911111111112
+//	     01234567890
+
+int seq[7] = {92, 39, 34, 36, 124, 60, 62};
+
+int	clean_arg(char *s)
 {
 	t_boolean sq;
 	t_boolean dq;
 	int i;
 	int j;
 
-	j = 0;
+	i = 0;
 	sq = FALSE;
 	dq = FALSE;
-	while (args[++j])
+	while (s[i])
 	{
-		while (arg[j][i])
+		if (!sq && !sq && s[i] == 92) // si no mod et escape char escape all
 		{
-			i = 0;
-			sq = arg[j][i] == '\'' ? !sq : sq;
-			dq = arg[j][i] == '"' ? !dq : dq;
-			if (arg[j][i] == '\\')
-				escape_char(&arg[j][0], &arg[j][i], sq, dq);
-//			else if (*arg[i] == '$')
-//				expand_var(sq, dq);
-			i++;
+			j = 0;
+			while (j < 7 && seq[j] != s[i + 1])
+				j++;
+			if (j != 7)
+			{
+				ft_memmove(&s[i], &s[i + 1], ft_strlen(s) - i);
+				i++;
+			}
 		}
+		else if (!sq && !dq && s[i] == 39) // si dq FALSE, sq FALSE, on active et on clear, l'escape n'existe pas en sq mode 
+		{
+			sq = TRUE;
+			ft_memmove(&s[i], &s[i + 1], ft_strlen(s) - i);
+		}
+		else if (sq && !dq && s[i] == 39) // si sq TRUE, dq FALSE, on desactive et on clear, l'escape n'existe pas en sq mode
+		{
+			sq = FALSE;
+			ft_memmove(&s[i], &s[i + 1], ft_strlen(s) - i);
+		}
+		else if (!sq && !dq && s[i] == 34 && (i == 0 || s[i - 1] != 92)) // si sqdq FALSE et single quotes et index = 0 ou index -1 diff de escapechar, on active et on clear 
+		{
+			dq = TRUE;
+			ft_memmove(&s[i], &s[i + 1], ft_strlen(s) - i);
+		}
+		else if (dq && !sq && s[i] == 34 && s[i - 1] != 92) // si sq TRUE et dq FALSE et pas d'escape derriere on desactive et on clear
+		{
+			dq = FALSE;
+			ft_memmove(&s[i], &s[i + 1], ft_strlen(s) - i);
+		}
+		else
+			i++;
 	}
+	if (sq || dq) // si sq ou dq TRUE la matching quote n'as pas ete trouvee donc syntx error
+		return (1);
+	return (0);
+}
+
+int	foreach_escape(char **args)
+{
+	int i;
+
+	i = 0;
+	while (args[++i])
+		clean_arg(args[i]);
 	return (0);
 }
