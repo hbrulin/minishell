b40afc18938760157c16e6ff4b8153e85e354506
pmouhali <p.m.272019@gmail.com>
2020-02-07 17:41:30 +0100

    escaping part ok + specs

--- interpreter.c
+++ interpreter.c
@@ -1,29 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   expand_and_escape.c                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pmouhali <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/05 17:32:25 by pmouhali          #+#    #+#             */
-/*   Updated: 2020/02/06 16:14:24 by pmouhali         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
 /*
-**	Needs escape_sequences header
 **	Receive splitted arguments.
 **	args[0] = cmd name
 **	DR replace by corresponding env variable
 **	BS replace by corresponding char
 **	Check for syntax error
 */
-
-// hello"hello\"""hello"
-// 012345678911111111112
-//	     01234567890
 
 int seq[7] = {'\\', '$', '\"', '\'', '|', '<', '>'};
 
@@ -41,11 +24,7 @@ int	clean_arg(char *s)
 	{
 		if (!sq && !dq && s[i] == 92) // si no mod et escape char escape all
 		{
-			j = 0;
-			while (j < 7 && seq[j] != s[i + 1])
-				j++;
-			if (j != 7)
-				ft_memmove(&s[i], &s[i + 1], ft_strlen(s) - i);
+			ft_memmove(&s[i], &s[i + 1], ft_strlen(s) - i);
 			i++;
 		}
 		else if (!sq && dq && s[i] == 92) // si dq et escape char escape all
@@ -55,7 +34,7 @@ int	clean_arg(char *s)
 				j++;
 			if (j != 3)
 				ft_memmove(&s[i], &s[i + 1], ft_strlen(s) - i);
-			i = j == 0 ? i + 2 : i + 1;
+			i = j == 1 ? i + 2 : i + 1;
 		}
 		else if (!sq && !dq && s[i] == 39) // si dq FALSE, sq FALSE, on active et on clear, l'escape n'existe pas en sq mode 
 		{
---
+++ specs/interpreter_spec.c
@@ -0,0 +1,102 @@
+#include "../minishell.h"
+#include <string.h>
+#include <stdio.h>
+
+void	test_simple_arg(const char *arg, const char *expected_arg, int expected_ret)
+{
+		char *test;
+		int ret;
+
+		if (!(test = strdup(arg)))
+		{ printf("Error: could't allocate memory for test.\n"); return ; }
+		ret = clean_arg(test);
+		if (ret == expected_ret && strcmp(test, expected_arg) == 0)
+			printf(".");
+		else if (ret == expected_ret && expected_ret == 1)
+			printf(".");
+		else
+		{
+			printf(
+				"\nFor arg %s :\nexpected %d got %d\nexpected %s got %s\n",
+				arg, expected_ret, ret, expected_arg, test
+			);
+		}
+		free(test);
+		return ;
+}
+
+int		main(void)
+{
+	// NO QUOTES
+	test_simple_arg("unicorn", "unicorn", 0);	
+	test_simple_arg("\\unicorn", "unicorn", 0);	
+	test_simple_arg("\\unicorn\\", "unicorn", 0);	
+	test_simple_arg("\\", "", 0);	
+	test_simple_arg("\\uni\\\\corn", "uni\\corn", 0);	
+	test_simple_arg("\\\\", "\\", 0);	
+	test_simple_arg("\\u\\n\\i\\c\\o\\r\\n", "unicorn", 0);	
+	test_simple_arg("\\\"", "\"", 0);	
+	test_simple_arg("\\$PWD", "$PWD", 0);	
+	test_simple_arg("\\\"uni\\'corn\\'\\\"", "\"uni'corn'\"", 0);	
+	test_simple_arg("\\\"\\'\\\"\\'\\\"\\'", "\"'\"'\"'", 0);	
+	test_simple_arg("\\\\\\\\\\\\\\\\", "\\\\\\\\", 0);	
+	test_simple_arg("\"\"\"", "", 1);	
+	test_simple_arg("\"", "", 1);	
+	test_simple_arg("'", "", 1);	
+	test_simple_arg("'''", "", 1);	
+	test_simple_arg("\"\"unicorn\"", "", 1);	
+	test_simple_arg("unicorn\"", "", 1);	
+	test_simple_arg("uni\"corn", "", 1);	
+	test_simple_arg("uni'", "", 1);	
+	test_simple_arg("'unicorn''", "", 1);	
+	test_simple_arg("unicorn\\\\\"", "", 1);	
+	test_simple_arg("uni\\\\\"corn", "", 1);	
+	test_simple_arg("uni\\'\\", "uni'", 0);	
+	test_simple_arg("'uni\\corn'\\\\'", "", 1);	
+	// DOUBLE QUOTES
+	test_simple_arg("\"unicorn\"", "unicorn", 0);	
+	test_simple_arg("\"\\unicorn\"", "\\unicorn", 0);	
+	test_simple_arg("\"\\unicorn\\\"", "", 1);	
+	test_simple_arg("\"\\\"", "", 1);	
+	test_simple_arg("\"\\uni\\\\corn\"", "\\uni\\corn", 0);	
+	test_simple_arg("\"\\\\\"", "\\", 0);	
+	test_simple_arg("\"\\u\\n\\i\\c\\o\\r\\n\"", "\\u\\n\\i\\c\\o\\r\\n", 0);	
+	test_simple_arg("\"\\\"\"", "\"", 0);	
+	test_simple_arg("\"\\$PWD\"", "$PWD", 0);	
+	test_simple_arg("\"\\\"uni\\'corn\\'\\\"\"", "\"uni\\'corn\\'\"", 0);	
+	test_simple_arg("\"\\\"\\'\\\"\\'\\\"\\'\"", "\"\\'\"\\'\"\\'", 0);	
+	test_simple_arg("\"\\\\\\\\\\\\\\\\\"", "\\\\\\\\", 0);	
+	test_simple_arg("\"          \"", "          ", 0);	
+	test_simple_arg("\"\"\"\"\"", "", 1);
+	test_simple_arg("\"\\\"\"", "\"", 0);
+	test_simple_arg("\"\\\"\\\"\"", "\"\"", 0);
+	test_simple_arg("\"'\"", "'", 0);	
+	test_simple_arg("\"'''\"", "'''", 0);	
+	test_simple_arg("\"\"\"unicorn\"\"", "", 1);	
+	test_simple_arg("\"unicorn\"\"", "", 1);	
+	test_simple_arg("\"uni\"corn\"", "", 1);	
+	test_simple_arg("\"uni'\"", "uni'", 0);	
+	test_simple_arg("\"'unicorn''\"", "'unicorn''", 0);	
+	test_simple_arg("\"unicorn\\\\\"\"", "", 1);
+	test_simple_arg("\"uni\\\\\"corn\"", "", 1);	
+	test_simple_arg("\"uni\\'\\\"", "", 1);	
+	test_simple_arg("\"uni\\'\"", "uni\\'", 0);	
+	test_simple_arg("\"'uni\\corn'\\\\'\"", "'uni\\corn'\\'", 0);	
+	// SINGLE QUOTES
+	test_simple_arg("'unicorn'", "unicorn", 0);	
+	test_simple_arg("'\\unicorn'", "\\unicorn", 0);	
+	test_simple_arg("'\\unicorn\\'", "\\unicorn\\", 0);	
+	test_simple_arg("'\\uni\\\\corn'", "\\uni\\\\corn", 0);	
+	test_simple_arg("'\\$PWD'", "\\$PWD", 0);
+	test_simple_arg("'\\\"uni\\'corn\\'\\\"'", "", 1);	
+	test_simple_arg("'\\\"\\'\\\"\\'\\\"\\''", "", 1);	
+	test_simple_arg("'\\\\\\\\\\\\\\\\'", "\\\\\\\\\\\\\\\\", 0);	
+	test_simple_arg("'''", "", 1);	
+	test_simple_arg("'''''", "", 1);	
+	test_simple_arg("'''unicorn", "", 1);	
+	test_simple_arg("'''unicorn'''unicorn''unicorn'unicorn''", "", 1);	
+	test_simple_arg("'\\''", "", 1);	
+	test_simple_arg("''\\'\\\\\\\\", "'\\\\", 0);
+	test_simple_arg("''\\'\\\\\\", "'\\", 0);
+	printf("\n");
+}
