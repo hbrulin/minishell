2e5a0664dc75162c321f7d00d5489300ad8317e1
Helene BRULIN <hbrulin@e2r4p11.42.fr>
2020-02-13 19:40:14 +0100

    pipes work for real

--- srcs/pipes.c
+++ srcs/pipes.c
@@ -6,14 +6,14 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 18:56:35 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/02/13 19:31:47 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/02/13 19:39:38 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h>
 
-int run_pipe(char **a_cmd, int *fd)
+int run_pipe(char **a_cmd, int *fd, int next)
 {
 	static int	fd_in;
 	pid_t		pid;
@@ -25,7 +25,8 @@ int run_pipe(char **a_cmd, int *fd)
 	if (pid == 0)
 	{
 		dup2(fd_in, 0);
-		dup2(1, fd[1]);
+		dup2((next) ? fd[1] : -1, 1);
+		close(fd[1]);
 		if(run_dmc(a_cmd))
 		{
 			exit(EXIT_FAILURE);
@@ -36,18 +37,18 @@ int run_pipe(char **a_cmd, int *fd)
 	else
 	{
 		wait(&status);
-		fd_in = fd[1];
+		close(fd[1]);
+		fd_in = fd[0];
 	}
 	return (0);
 }
-
 
 int	run_dmc_pipes(char **args)
 {
 	int fd[2];
 	char **a_cmd;
-	//char **b_cmd;
 	int i;
+	int next = 0;
 
 	int adv;
 
@@ -61,23 +62,18 @@ int	run_dmc_pipes(char **args)
 				a_cmd = ft_sub_tab(args, adv, i);
 			else
 				a_cmd = ft_sub_tab(args, adv, adv + (ft_tab_chr_i((char **)&args[i + 1], "|") - 1));
-			//else
-			//	b_cmd = ft_sub_tab(args, i + 1, i + (ft_tab_chr_i((char **)&args[i + 1], "|") - 1));
-			//pipe(fd);
 			adv = i + 1;
 			pipe(fd);
-			if (run_pipe(a_cmd, fd))
+			next = 1;
+			if (run_pipe(a_cmd, fd, next))
 				return(1);
-			//printf("%i - a:\n", i);
-			//ft_tab_print(a_cmd);		
-			//free(a_cmd);
-			//free(b_cmd);
 		}
 		else if (ft_iter_tab_cmp((char **)&args[i + 1], "|"))
 		{
 			a_cmd = ft_sub_tab(args, i, ft_tablen(args) - i);
 			pipe(fd);
-			if (run_pipe(a_cmd, fd))
+			next = 0;
+			if (run_pipe(a_cmd, fd, next))
 				return(1);
 			break;
 			//printf("%i - a:\n", i);
