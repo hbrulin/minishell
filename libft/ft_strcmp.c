12b2b9a5779fefbbc2d3184afa6826eab618de5d
Helene BRULIN <hbrulin@e2r3p15.42.fr>
2020-02-19 12:39:59 +0100

    export works again

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 13:25:14 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/02/14 20:02:04 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/02/19 12:38:17 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	if (ft_strlen(s2) != ft_strlen(s1))
-		return (1);
 	return (ft_memcmp(s1, s2, ft_strlen(s1) + 1));
 }
