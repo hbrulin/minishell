1134293ee98a33aa37c73a287c202a6c0d182c3c
Helene BRULIN <hbrulin@e2r3p15.42.fr>
2020-02-19 17:07:38 +0100

    norm parse_cmds almost ok

--- srcs/parse_cmds.c
+++ srcs/parse_cmds.c
@@ -6,7 +6,7 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 16:33:00 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/02/19 16:44:42 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/02/19 17:07:13 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int		check_error(char *s)
 {
 	int i;
 
+	if (!s)
+		return (1);
 	i = 0;
 	while (s[i])
 	{
@@ -40,16 +42,9 @@ void	set_quote(char c, t_parse_tools *t)
 		t->open = !t->open;
 }
 
-int		parse_cmds(char *s)
+int		malloc_and_exec(char *cmd, char *s, t_parse_tools *t, int flag)
 {
-	t_parse_tools	t;
-	char			*cmd;
-
-	cmd = NULL;
-	ft_bzero(&t, sizeof(t_parse_tools));
-	if (!s)
-		return (0);
-	if (ft_strchr(s, ';') == NULL)
+	if (flag == 1)
 	{
 		if (!(cmd = ft_strdup(s)))
 			return (g_ret = ft_strerror(NULL, NULL, NULL, NULL));
@@ -57,27 +52,51 @@ int		parse_cmds(char *s)
 			return (1);
 		return (0);
 	}
+	if (flag == 2)
+	{
+		if (!(cmd = ft_strdup(s + t->j)))
+			return (g_ret = ft_strerror(NULL, NULL, NULL, NULL));
+		parsexec(cmd);
+		return (0);
+	}
+	if (flag == 3)
+	{
+		if (!(cmd = ft_substr(s, t->j, t->i - t->j)))
+			return (g_ret = ft_strerror(NULL, NULL, NULL, NULL));
+		parsexec(cmd);
+		return (0);
+	}
+	return (1);
+}
+
+int		parse_cmds(char *s)
+{
+	t_parse_tools	t;
+	char			*cmd;
+
+	cmd = NULL;
+	ft_bzero(&t, sizeof(t_parse_tools));
 	if (check_error(s))
 		return (1);
+	if (ft_strchr(s, ';') == NULL)
+		return (malloc_and_exec(cmd, s, &t, 1));
 	while (s[t.i])
 	{
 		if (ft_strchr(s + t.i, ';') == NULL)
 		{
-			if (!(cmd = ft_strdup(s + t.j)))
-				return (g_ret = ft_strerror(NULL, NULL, NULL, NULL));
-			parsexec(cmd);
+			if (malloc_and_exec(cmd, s, &t, 2))
+				return (1);
 			break ;
 		}
 		set_quote(s[t.i], &t);
 		if (s[t.i] == ';' && t.open == 0)
 		{
-			if (!(cmd = ft_substr(s, t.j, t.i - t.j)))
-				return (g_ret = ft_strerror(NULL, NULL, NULL, NULL));
+			if (malloc_and_exec(cmd, s, &t, 3))
+				return (1);
 			t.i++;
 			while (ft_is_space(s[t.i]))
 				t.i++;
 			t.j = t.i;
-			parsexec(cmd);
 		}
 		t.i++;
 	}
