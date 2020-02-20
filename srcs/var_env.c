6692565782ab189e7cef268997f12c489decc0b2
Helene BRULIN <hbrulin@e2r6p14.42.fr>
2020-02-20 17:27:07 +0100

    norme var_env

--- srcs/var_env.c
+++ srcs/var_env.c
@@ -6,13 +6,13 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/08 11:09:00 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/02/20 16:16:35 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/02/20 17:26:43 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char    *get_var(t_list *lst, char *key)
+char	*get_var(t_list *lst, char *key)
 {
 	t_list *next;
 
@@ -20,19 +20,19 @@ char    *get_var(t_list *lst, char *key)
 	{
 		next = lst->next;
 		if (!(ft_strncmp(lst->content, key, (int)ft_strlen(key))))
-			return(ft_strdup((char *)&(lst->content)[ft_strlen(key)]));
+			return (ft_strdup((char *)&(lst->content)[ft_strlen(key)]));
 		lst = next;
 	}
-	return(NULL);
+	return (NULL);
 }
 
 int		set_var_full(t_list *lst, char *key, char *value)
 {
-	t_list *next;
-	char *tmp;
-	char *new;
-	
-	if(!key || !(new = ft_strdup(value)))
+	t_list	*next;
+	char	*tmp;
+	char	*new;
+
+	if (!key || !(new = ft_strdup(value)))
 		return (-1);
 	while (lst)
 	{
@@ -49,23 +49,19 @@ int		set_var_full(t_list *lst, char *key, char *value)
 	return (1);
 }
 
-
 int		set_var(t_list *lst, char *key, char *value)
 {
-	t_list *next;
-	char *tmp;
-	char *new;
+	t_list	*next;
+	char	*tmp;
+	char	*new;
 
 	if (!value)
 	{
-		if(!(new = ft_strdup(key)))
+		if (!(new = ft_strdup(key)))
 			return (-1);
 	}
-	else
-	{
-		if(!(new = ft_strjoin(key, value)))
-			return (-1);
-	}
+	else if (!(new = ft_strjoin(key, value)))
+		return (-1);
 	while (lst)
 	{
 		next = lst->next;
@@ -80,7 +76,7 @@ int		set_var(t_list *lst, char *key, char *value)
 	return (1);
 }
 
-t_list    *del_var(t_list *lst, char *key)
+t_list	*del_var(t_list *lst, char *key)
 {
 	t_list	*tmp;
 	t_list	*previous;
@@ -89,22 +85,22 @@ t_list    *del_var(t_list *lst, char *key)
 	previous = lst;
 	if (!(ft_strncmp(previous->content, key, ft_strlen(key))))
 	{
-  		lst = previous->next;
-  		ft_lstdelone(previous, free);
-  		return (lst);
+		lst = previous->next;
+		ft_lstdelone(previous, free);
+		return (lst);
 	}
 	tmp = previous->next;
-	while(tmp != NULL)
+	while (tmp != NULL)
 	{
 		ret = ft_strncmp(tmp->content, key, ft_strlen(key));
-  		if (ret == 0)
-  		{
-    		previous->next = tmp->next;
-    		ft_lstdelone(tmp, free);
-    		return (lst);
-  		}
-  		previous = tmp; 
-  		tmp = tmp->next;
+		if (ret == 0)
+		{
+			previous->next = tmp->next;
+			ft_lstdelone(tmp, free);
+			return (lst);
+		}
+		previous = tmp;
+		tmp = tmp->next;
 	}
 	return (lst);
 }
