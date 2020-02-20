c5bb51732686e2c41789d40f6a30c9835b85701c
Helene BRULIN <hbrulin@e2r6p14.42.fr>
2020-02-20 14:22:15 +0100

    error msg redirect

--- srcs/redirect.c
+++ srcs/redirect.c
@@ -6,7 +6,7 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 16:31:29 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/02/20 14:13:44 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/02/20 14:22:04 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		redirect_right(char *type, char *path_to)
 	else if (ft_strcmp(type, ">>") == 0)
 		fd = open(path_to, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (fd < 0)
-		return(ft_error("minishell: %s: No such file or directory\n", NULL, NULL, path_to));
+		return(ft_error("minishell: %s: No such file\n", NULL, NULL, path_to));
 	dup2(fd, 1);
 	close(fd);
 	return (0);
@@ -38,7 +38,7 @@ int		redirect_left(char *path_from) //NE PAS EXECUTER LA CMD 1 -> revoir ca -> m
 	int		fd; 
 
 	if ((fd = open(path_from, O_RDONLY)) < 0)
-		return(ft_error("minishell: %s: No such file or directory\n", NULL, NULL, path_from));
+		return(ft_error("minishell: %s: No such file or is a directory\n", NULL, NULL, path_from));
 	dup2(fd, 0);
 	close(fd);
 	return (0);
@@ -50,7 +50,7 @@ int		ft_create(char *path)
 
 	fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd < 0)
-		return(ft_error("minishell: %s: Error\n", NULL, NULL, path));
+		return(ft_error("minishell: %s: File could not be created\n", NULL, NULL, path));
 	return (0);
 }
 
