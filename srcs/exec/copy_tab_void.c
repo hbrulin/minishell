5d76df5982c56268d2f0f297f91122e9b70e81d4
Helene BRULIN <hbrulin@e1r9p6.42.fr>
2020-02-25 20:03:34 +0100

    ls

---
+++ srcs/exec/copy_tab_void.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   copy_tab_void.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/25 19:50:14 by hbrulin           #+#    #+#             */
+/*   Updated: 2020/02/25 19:56:10 by hbrulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+size_t		ft_tablen_void(char **tab)
+{
+	size_t	count;
+	int		i;
+
+	i = 0;
+	count = 0;
+	if (tab)
+	{
+		while (tab[i])
+		{
+			if (ft_strcmp(tab[i], ""))
+				count++;
+			i++;
+		}
+	}
+	return (count);
+}
+
+char	**copy_tab_void(char **tab)
+{
+	char	**cpy;
+	char	**cpy_ret;
+	size_t	len;
+	int		i;
+
+	i = 0;
+	if (!(len = ft_tablen_void(tab)))
+		return (NULL);
+	if (!(cpy = (char**)malloc(sizeof(char*) * (len + 1))))
+		return (NULL);
+	cpy_ret = cpy;
+	cpy[len] = NULL;
+	len = 0;
+	while (tab[i])
+	{
+		if (ft_strcmp(tab[i], ""))
+		{
+			if (!(cpy[len] = ft_strdup(tab[i])))
+				return (NULL);
+			len++;
+		}
+		i++;
+	}
+	return (cpy_ret);
+}
