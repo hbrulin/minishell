da30e6dc9b145032515e0d0fe30a80110c80c193
Helene BRULIN <hbrulin@e1r6p3.42.fr>
2020-02-28 17:00:04 +0100

    fix leak ls_part

--- srcs/parsing/ls_part.c
+++ srcs/parsing/ls_part.c
@@ -6,17 +6,20 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 15:37:18 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/02/28 16:32:10 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/02/28 16:59:45 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+
 
 int		ls_part(char **args)
 {
 	int i = 0;
 	int j;
 	char *tmp;
+	char *var;
 
 	if (ft_tablen(args) < 2)
 		return (0);
@@ -28,19 +31,20 @@ int		ls_part(char **args)
 	}
 	else if (args[1][0] == '\"' && args[1][1] == '$')
 	{
-			j = 1;
-			tmp = ft_strdup(args[1]);
-			while (args[1][j] != '\"' && args[1][j])
-				j++;
-			tmp[j] = '=';
-			ft_memmove(&(tmp)[0], &(tmp)[2], ft_strlen(tmp));
-			if (!get_var(g_env, tmp))
-			{
-				g_ret = 1;
-				ft_printf_fd(2, "ls: : No such file or directory\n");
-				return (ft_error(NULL, tmp, NULL, NULL));
-			}
-			free(tmp);
+		j = 1;
+		tmp = ft_strdup(args[1]);
+		while (args[1][j] != '\"' && args[1][j])
+			j++;
+		tmp[j] = '=';
+		ft_memmove(&(tmp)[0], &(tmp)[2], ft_strlen(tmp));
+		if (!(var = get_var(g_env, tmp)))
+		{
+			g_ret = 1;
+			ft_printf_fd(2, "ls: : No such file or directory\n");
+			return (ft_error(NULL, tmp, NULL, NULL));
+		}
+		free(tmp);
+		free(var);
 	}
 	while (args[i])
 	{
@@ -57,12 +61,14 @@ int		ls_part(char **args)
 				j++;
 			tmp[j] = '=';
 			ft_memmove(&(tmp)[0], &(tmp)[2], ft_strlen(tmp));
-			if (!get_var(g_env, tmp))
+			if (!(var = get_var(g_env, tmp)))
 			{
 				g_ret = 1;
 				ft_printf_fd(2, "ls: : No such file or directory\n");
 			}
 			free(tmp);
+			if (var)
+				free(var);
 		}
 		i++;
 	}
