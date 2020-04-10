930ee874f866fa3c3b3c5b59dea19c2f4274c6e1
babaoriley <helenebrulin@live.fr>
2020-04-10 19:42:26 +0200

    norm continuing

--- srcs/fds/redir_pipes.c
+++ srcs/fds/redir_pipes.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redir_pipes.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/10 19:36:39 by hbrulin           #+#    #+#             */
+/*   Updated: 2020/04/10 19:41:45 by hbrulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int		count_redir(char **args)
+int			count_redir(char **args)
 {
 	int i;
 	int count;
@@ -9,7 +21,8 @@ int		count_redir(char **args)
 	count = 0;
 	while (args[i])
 	{
-		if (!(ft_strcmp(args[i], "<")) || !(ft_strcmp(args[i], ">")) || !(ft_strcmp(args[i], ">>")) )
+		if (!(ft_strcmp(args[i], "<")) || !(ft_strcmp(args[i], ">"))
+			|| !(ft_strcmp(args[i], ">>")))
 			count++;
 		i++;
 	}
@@ -19,44 +32,48 @@ int		count_redir(char **args)
 t_redir		*make_tmp(int dir, char *file)
 {
 	t_redir *tmp;
+
 	tmp = malloc(sizeof(t_cmd));
 	tmp->direction = dir;
 	tmp->name = ft_strdup(file);
 	return (tmp);
 }
 
-t_redir **build_redir(char ** a_cmd)
+t_redir		**build_redir(char **a_cmd)
 {
 	t_redir **redirs;
-	int				len;
-    int i = 0;
-    int j = 0;
+	int		len;
+	int		i;
+	int		j;
 
-    if (!(ft_tab_chr(a_cmd, "<")) && !(ft_tab_chr(a_cmd, ">")) && !(ft_tab_chr(a_cmd, ">>")))
-        return (NULL);
-
+	i = 0;
+	j = 0;
+	if (!(ft_tab_chr(a_cmd, "<")) && !(ft_tab_chr(a_cmd, ">"))
+		&& !(ft_tab_chr(a_cmd, ">>")))
+		return (NULL);
 	len = count_redir(a_cmd) + 1;
 	redirs = malloc(sizeof(t_cmd) * len);
-
-    while (a_cmd[i])
+	while (a_cmd[i])
 	{
 		if ((ft_strcmp(a_cmd[i], ">") == 0 && ft_strcmp(a_cmd[i - 1], "\\")))
 		{
-            redirs[j] = make_tmp(OUTPUT, a_cmd[i + 1]);
-            j++;
+			redirs[j] = make_tmp(OUTPUT, a_cmd[i + 1]);
+			j++;
 		}
-        else if ((ft_strcmp(a_cmd[i], ">>") == 0 && ft_strcmp(a_cmd[i - 1], "\\")))
+		else if ((ft_strcmp(a_cmd[i], ">>") == 0 && ft_strcmp(a_cmd[i - 1],
+			"\\")))
 		{
 			redirs[j] = make_tmp(APPEND, a_cmd[i + 1]);
-            j++;
+			j++;
 		}
-		else if ((ft_strcmp(a_cmd[i], "<") == 0 && ft_strcmp(a_cmd[i - 1], "\\")))
+		else if ((ft_strcmp(a_cmd[i], "<") == 0 && ft_strcmp(a_cmd[i - 1],
+			"\\")))
 		{
 			redirs[j] = make_tmp(INPUT, a_cmd[i + 1]);
-            j++;
+			j++;
 		}
 		i++;
 	}
-    redirs[j] = NULL;
-    return (redirs);
+	redirs[j] = NULL;
+	return (redirs);
 }
