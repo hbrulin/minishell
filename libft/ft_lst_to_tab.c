3366332a62c06a3e226e9ab62573a0b71ccaa6e9
babaoriley <helenebrulin@live.fr>
2020-04-12 16:20:05 +0200

    fix error valgrind lst_to_tab

--- libft/ft_lst_to_tab.c
+++ libft/ft_lst_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 11:17:41 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/02/13 13:38:36 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/04/12 16:19:32 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ char	**ft_lst_to_tab(t_list *list)
 	t_list	*tmp;
 	int		i;
 
-	if (!(ret = (char**)malloc(sizeof(char *) * ft_lstsize(list) + 1)))
-		return (NULL);
+	if (!(ret = malloc((ft_lstsize(list) + 1) * sizeof(*ret))))
+        return (NULL);
 	i = 0;
 	tmp = list;
 	while (tmp)
